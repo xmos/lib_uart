e40674ed52ed8499b69257b5b460b3cf417502b5
David Lacey <david.lacey@xmos.com>
2014-12-09 09:34:45 +0000

    Update tests to new xmostest API

--- tests/runtests.py
+++ tests/runtests.py
@@ -17,14 +17,27 @@ xmostest.build("app_uart_test")
 
 resources = xmostest.request_resource("xsim")
 
-tester = xmostest.extract_test_results("lib_uart", "sim_regression")
+class UartTester(xmostest.Tester):
+
+    def __init__(self):
+        super(xmostest.Tester, self).__init__()
+        self.register_test("lib_uart","sim_regression","loopback_test", {})
+
+    def run(self, output):
+        result = True
+        for line in output:
+            if line.find("FAIL") != -1:
+                result = False
+        xmostest.set_test_result("lib_uart", "sim_regression", "loopback_test",
+                                 {}, result, output=''.join(output))
+
+
 
 xmostest.run_on_simulator(resources['xsim'],
                           'app_uart_test/bin/smoke/app_uart_test_smoke.xe',
                           xscope_io=True,
                           loopback=[{'from':'tile[0]:XS1_PORT_1A',
                                      'to':'tile[1]:XS1_PORT_1B'}],
-                          tester = tester)
-
+                          tester = UartTester())
 
 xmostest.finish()
