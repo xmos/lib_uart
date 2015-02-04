ba7b517eee1acb3a22db986642d5d18925fc227e
Simon Cooksey <sjc80@kent.ac.uk>
2015-02-04 10:21:08 +0000

    Refactor tests out into separate files

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,43 +1,25 @@
 #!/usr/bin/env python
 import xmostest
 
-xmostest.init()
+if __name__ == "__main__":
+    xmostest.init()
 
-xmostest.register_group("lib_uart",
-                        "sim_regression",
-                        "Uart Simulator Regression",
-"""
-Several tests are performed in simulation with a loopback between the UART Tx
-and Rx ports. This tests the features of the individual components,
-verifying them against each other. The various options and use cases of the
-components are tested.
-""")
+    xmostest.register_group("lib_uart",
+                            "sim_regression",
+                            "Uart Simulator Regression",
+    """
+    Several tests are performed in simulation with a loopback between the UART Tx
+    and Rx ports. This tests the features of the individual components,
+    verifying them against each other. The various options and use cases of the
+    components are tested.
+    """)
 
-xmostest.build("app_uart_test")
-
-resources = xmostest.request_resource("xsim")
-
-class UartTester(xmostest.Tester):
-
-    def __init__(self):
-        super(UartTester, self).__init__()
-        self.register_test("lib_uart","sim_regression","loopback_test", {})
-
-    def run(self, output):
-        result = True
-        for line in output:
-            if line.find("FAIL") != -1:
-                result = False
-        xmostest.set_test_result("lib_uart", "sim_regression", "loopback_test",
-                                 {}, result, output=''.join(output))
+    xmostest.register_group("lib_uart", "sim_regression", "Uart Transmission Test",
+    """
+    """)
 
 
+    xmostest.runtests()
 
-xmostest.run_on_simulator(resources['xsim'],
-                          'app_uart_test/bin/smoke/app_uart_test_smoke.xe',
-                          xscope_io=True,
-                          loopback=[{'from':'tile[0]:XS1_PORT_1A',
-                                     'to':'tile[1]:XS1_PORT_1B'}],
-                          tester = UartTester())
 
-xmostest.finish()
+    xmostest.finish()
