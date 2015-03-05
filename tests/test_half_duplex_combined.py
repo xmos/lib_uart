5bac154b636598bb9cb4cba9e98b34c1f4391dc1
David Lacey <david.lacey@xmos.com>
2015-03-05 14:17:56 +0000

    Update tests to use new xmostest build method

--- tests/test_half_duplex_combined.py
+++ tests/test_half_duplex_combined.py
@@ -20,14 +20,14 @@ def do_test(baud, parity):
     if not tester.test_required():
         return
 
-    xmostest.build(path, env=myenv, do_clean=True)
-
     xmostest.run_on_simulator(resources['xsim'],
                               'app_uart_test_half_duplex_combined/bin/smoke/app_uart_test_half_duplex_combined_smoke.xe',
                               simthreads=[hd_checker],
                               xscope_io=True,
                               tester=tester,
-                              simargs=["--vcd-tracing", "-tile tile[0] -ports -o trace.vcd"])
+                              simargs=["--vcd-tracing", "-tile tile[0] -ports -o trace.vcd"],
+                              clean_before_build=True,
+                              build_env=myenv)
 
 
 def runtests():
--- tests/test_rx_uart.py
+++ tests/test_rx_uart.py
@@ -19,14 +19,14 @@ def do_test(baud):
     if not tester.test_required():
         return
 
-    xmostest.build(path, env=myenv, do_clean=True)
-
     xmostest.run_on_simulator(resources['xsim'],
                               'app_uart_test_rx/bin/smoke/app_uart_test_rx_smoke.xe',
                               simthreads=[checker],
                               xscope_io=True,
                               tester=tester,
-                              simargs=["--vcd-tracing", "-tile tile[0] -ports -o trace.vcd"])
+                              simargs=["--vcd-tracing", "-tile tile[0] -ports -o trace.vcd"],
+                              clean_before_build=True,
+                              build_env=myenv)
 
 
 def runtests():
